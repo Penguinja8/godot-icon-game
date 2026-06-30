b2229d5b0d2af66fc2ba9e2550fd8a03e8d1afdf
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-06-30 11:46:03 -0400

    song stuff

--- song/sound.gd
+++ song/sound.gd
@@ -2,16 +2,20 @@ extends AudioStreamPlayer
 
 var playback: AudioStreamPlayback
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+
+func play_sound(dur: float = 0.2):
+	set_stream(load("uid://dbuxt1ud34xs4"))
+	play()
+	playback = get_stream_playback()
 	var img_data = []
 	var img: Image = load("res://icon.svg").get_image()
 	for x in range(img.get_width()):
 		for y in range(img.get_height()):
 			var pixel: Color = img.get_pixel(x,y)
 			img_data.append(pixel.b + pixel.g + pixel.r)
-	playback = get_stream_playback()
 	for i in range(playback.get_frames_available()):
 		if i >= len(img_data):
 			break
 		playback.push_frame(Vector2.ONE*img_data[i])
+	await get_tree().create_timer(dur).timeout
+	stop()
