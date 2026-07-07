8b49bbae952437ecc7250b4bdd9a2c3b67011702
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-07-07 12:16:21 -0400

    teleporting

--- components/teleporter.gd
+++ components/teleporter.gd
@@ -1,8 +1,16 @@
 extends Area2D
 class_name Teleporter
 
+## make it orange, else make it blue
+@export var orange: bool = false
 @export var destination_teleporter: Teleporter
+@onready var cooldown = $ExitCooldown
 
+func _ready():
+	if orange:
+		$Sprite2D.material.shader = load("res://components/teleporter_orange.gdshader")
 
 func _on_body_entered(body: Node2D) -> void:
-	body.global_position = destination_teleporter.global_position
+	if cooldown.is_stopped():
+		destination_teleporter.cooldown.start()
+		body.global_position = destination_teleporter.global_position
