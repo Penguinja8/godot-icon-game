2a02178e55567f8728a37c4ba40c639938f0ff3b
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-07-15 12:12:58 -0400

    dh

--- main/level_complete_screen.gd
+++ main/level_complete_screen.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@export var colors: Array[Color]
+
 func _ready():
 	var clear_time = GameState.get_level_timer()
 	if GameState.current_level_name not in GameState.best_times.keys():
@@ -8,7 +10,15 @@ func _ready():
 		if clear_time < GameState.best_times[GameState.current_level_name]:
 			GameState.best_times[GameState.current_level_name] = clear_time
 	$Time.text = "Clear time: " + str(snappedf(clear_time, 0.01))
-
+	if GameState.current_level_name not in GameState.MEDAL_TIMES:
+		return
+	var time_index = 4
+	var medal_times = GameState.MEDAL_TIMES[GameState.current_level_name]
+	for i in range(len(medal_times)):
+		if clear_time <= medal_times[i]:
+			time_index = i
+			break
+	$Medal.material.set_shader_parameter("base_color", colors[time_index])
 
 func _on_to_level_select_pressed() -> void:
 	get_tree().change_scene_to_file("res://main/level_select.tscn")
