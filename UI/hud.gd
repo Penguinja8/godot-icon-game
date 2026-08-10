20f5065abe140c527e26c235bddfee5bf11052f5
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2026-08-10 12:23:30 -0400

    fr this time

--- UI/hud.gd
+++ UI/hud.gd
@@ -3,4 +3,4 @@ extends CanvasLayer
 func _process(_delta):
 	var speed = snapped(abs(get_parent().velocity.x), 0.01)
 	$VBoxContainer/Speed.text = "Speed: " + ("%.2f" % speed) + " px/s"
-	$VBoxContainer/Stopwatch.text = "Time: " + str(snapped(GameState.level_timer, 0.01)) + " seconds"
+	$VBoxContainer/Stopwatch.text = "Time: " + ("%.2f" % snapped(GameState.level_timer, 0.01)) + " seconds"
